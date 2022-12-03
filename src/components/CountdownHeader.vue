6594e179b2e3c6c38f5b88a6ee0fab624fc4137d
bo7owers <7orres.rene@proton.me>
2022-12-03 14:40:06 -0500

    remove h2 for a11y

--- src/components/CountdownHeader.vue
+++ src/components/CountdownHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="text-center">
-    <h2 class="text-green text-2xl">
-      <span class="bg-gray-200 px-3.5 py-1 rounded text-sm font-bold">Advent of Vue</span>
-    </h2>
+    <p class="text-green text-2xl">
+      <span class="bg-gray-100 px-3.5 py-1 rounded text-sm font-bold">Advent of Vue</span>
+    </p>
     <h1 class="text-4xl mt-2 mb-5">Christmas Countdown</h1>
   </header>
 </template>
