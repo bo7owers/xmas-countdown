31e44d8953220324b8ddc01d3cace9ad42debfcc
bo7owers <7orres.rene@proton.me>
2022-12-03 13:44:12 -0500

    Tweak count down

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,30 @@
 import CountdownHeader from '@/components/CountdownHeader.vue'
 import CountdownSegment from './components/CountdownSegment.vue'
 import { useNow } from '@vueuse/core'
+import { computed } from 'vue'
 
 const now = useNow()
 const christmas = new Date('12/25/2022 00:00:00')
+let daylength = 1000 * 60 * 60 * 24
+
+console.log(christmas.getTime() - now.value.getTime() / daylength)
+
+const timeTo = computed(() => {
+  return christmas.getTime() - now.value.getTime() / daylength
+})
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
     <div>
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
+        <!-- {{ christmas }} | {{ now }} -->
+        {{ timeTo }}
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" number="00" />
-          <CountdownSegment label="hours" number="00" />
-          <CountdownSegment label="minutes" number="00" />
-          <CountdownSegment label="seconds" number="00" />
+          <CountdownSegment label="days" :number="0" />
+          <CountdownSegment label="hours" :number="0" />
+          <CountdownSegment label="minutes" :number="0" />
+          <CountdownSegment label="seconds" :number="0" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
